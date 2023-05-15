4b1ff52a919601ae857bcb6dab0b9a952f65c8af
Varun Doshi <61531351+varun-doshi@users.noreply.github.com>
2023-05-15 22:32:35 +0530

    Update HelloWorld.sol
    Added Comments

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,13 +3,21 @@
 pragma solidity ^0.8.18;
 
 contract helloWorld{
+
+// State variable called "text" to store string
     string public text;
+   
+//Constructor function to initialize value of variable.
     constructor(string memory _text){
         text=_text;
     }
+    
+//Function to set new value to variable    
     function setText(string memory newText) public{
         text=newText;
     }
+    
+////Function to fetch new value to variable (by default all public variables have this function)
     function getText() public view returns(string memory){
         return text;
     }
